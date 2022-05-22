e98713cfacc37ad0de4154dcfef94a55a5d3aa08
SAI <saitoh.hiroki.1974@gmail.com>
2022-05-23 08:12:25 +0900

    exe

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -35,11 +35,11 @@ namespace SecOcr
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(220, 151);
+            this.label1.Location = new System.Drawing.Point(204, 147);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 12);
+            this.label1.Size = new System.Drawing.Size(145, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Drag Drop Here!";
+            this.label1.Text = "Drag Drop Image File Here!";
             // 
             // FormMain
             // 
